502aa11f247c7a4117e759db2d0c66c866852bc5
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2024-01-05 15:03:18 -0400

    using useEffect with dependency array

--- 16-react-vite-useEfect/src/App.tsx
+++ 16-react-vite-useEfect/src/App.tsx
@@ -3,19 +3,27 @@ import './App.css'
 
 function App() {
 
-  const [starWarsData, setStarWarsData] = useState({})
+	const [starWarsData, setStarWarsData] = useState({})
+	const [count, setCount] = useState(1)
+
+	console.log('render outside')
 
   useEffect(()=>{
-    fetch("https://swapi.dev/api/people/1")
+    console.log('render inside')
+
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then(res => res.json())
       .then(data => setStarWarsData(data))
-  })
+	},[count])
+	//dependency array - contain values that if they change will cause the effect to run
 
-  return (
-    <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-    </div>
-  )
+	return (
+		<div>
+			<pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+			<h3>the count {count}</h3>
+			<button onClick={()=>setCount(prevCount=>prevCount+1)}>add</button>
+		</div>
+	)
 }
 
 export default App
