08d647b604c473f96f4d18cb46160cca3ebc5f41
Irvin Gomes <irvin.r.gomes@gmail.com>
2024-01-04 21:55:21 -0400

    creating form1 with two simple useState

--- 15-react-vite-Forms/src/App.tsx
+++ 15-react-vite-Forms/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import Form1 from "./components/Form1";
 
 function App() {
   return (
     <div>
-      app
+      <div className="formWrapper">
+        <Form1 />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 15-react-vite-Forms/src/components/Form1.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Form1: React.FC<any> = ({}) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  console.log(firstName, lastName)
+
+  const handleChangeFirst = (event: any) => {
+    setFirstName(event.target.value)
+  };
+  const handleChangeLast = (event: any) => {
+    setLastName(event.target.value)
+  };
+
+  return (
+    <form className="form">
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        className="form-input"
+        placeholder="First Name"
+        onChange={handleChangeFirst}
+      />
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        className="form-input"
+        placeholder="First Name"
+        onChange={handleChangeLast}
+      />
+    </form>
+  );
+};
+export default Form1;
