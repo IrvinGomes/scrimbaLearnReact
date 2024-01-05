b3c080df81b2a173e8c3f2cfe37d20bb6a558c4f
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2024-01-05 11:48:02 -0400

    signup form

--- 15-react-vite-Forms/src/App.tsx
+++ 15-react-vite-Forms/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Form1 from "./components/Form1";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
     <div>
       <div className="formWrapper">
         <Form1 />
+      </div>
+        <hr className="dashed"/>
+      <div className="formWrapper-signup">
+        <SignupForm />
       </div>
     </div>
   );
